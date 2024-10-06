189f1e4e4fa86b7eda122271cb1d52c8992ec12d
LeopoldHsing <LeopoldHsing@gmail.com>
2024-10-05 21:43:03 -0500

    add validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  # title, price, and stock_quantity
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
